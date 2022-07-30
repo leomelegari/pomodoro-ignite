76f1bf527d9a34334055f5173300f1ec4001d87e
Leonardo Melegari Deffende <leo_melegari@outlook.com>
2022-07-30 09:59:53 -0300

    feat: form working

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,7 @@
+import { FieldValues, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
 import { Play } from "phosphor-react";
 import {
   CountDownContainer,
@@ -9,22 +13,66 @@ import {
   TaskInput,
 } from "./styles";
 
+// Formulários
+
+// Controlled - manter em tempo real o valor inserido no estado do componente
+// é a forma padrão, e não errada, de pegar valores do input (aquele esquema de useState, onChange nos inputs, etc)
+// O problema deste método é que, quando um componente é atualizado/modificado, todos os componentes serão re-renderizados
+// causando uma queda de performance (quando se trata de um projeto muito grande, com muitos componentes)
+
+// Uncontrolled - bibliotecas externas como: react-hook-form
+
+const newCycleFormSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutes: zod
+    .number()
+    .min(5, "O ciclo precisa ser de, no mínimo, 5 minutos")
+    .max(60, "O ciclo precisa ser de até 60 minutos"),
+});
+
+// interface newCycleFormData {
+//   task: string;
+//   minutes: number;
+// }
+
+type NewCycleFormData = zod.infer<typeof newCycleFormSchema>;
+
 export const Home = () => {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormSchema),
+    // defaultValues: {
+    //   task: "",
+    //   minutes: 0,
+    // },
+  });
+
+  const onHandleSubmit = (data: NewCycleFormData) => {
+    console.log(data);
+    reset();
+  };
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(onHandleSubmit)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            {...register("task")}
+          />
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             id="minutesAmount"
             type="number"
             placeholder="00"
-            max={60}
             min={5}
-            
+            max={60}
+            {...register("minutes", { valueAsNumber: true })}
           />
           <span>minutos.</span>
         </FormContainer>
@@ -37,7 +85,7 @@ export const Home = () => {
           <span>0</span>
         </CountDownContainer>
 
-        <StartCountDown type="submit">
+        <StartCountDown disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountDown>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -27,7 +27,29 @@ export const FormContainer = styled.div`
   flex-wrap: wrap;
 `;
 
-const BaseInput = styled.input`
+export const TaskInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme["gray-500"]};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme["gray-100"]};
+  flex: 1;
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme["green-500"]};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme["gray-500"]};
+  }
+`;
+
+export const MinutesAmountInput = styled.input`
+  width: 4rem;
   background: transparent;
   height: 2.5rem;
   border: 0;
@@ -45,14 +67,6 @@ const BaseInput = styled.input`
   &::placeholder {
     color: ${(props) => props.theme["gray-500"]};
   }
-`;
-
-export const TaskInput = styled(BaseInput)`
-  flex: 1;
-`;
-
-export const MinutesAmountInput = styled(BaseInput)`
-  width: 4rem;
 `;
 
 export const CountDownContainer = styled.div`
